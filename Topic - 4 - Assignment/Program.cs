287094fc8bf3ae0dddcb538b515928fa8ead47c4
Teddy Becker <362687188@GoTVDSB.ca>
2026-09-23 13:19:49 -0400

    Add project files.

---
+++ Topic - 4 - Assignment/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Topic___4___Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            String Username;
+            int age;
+            double price;
+
+            Console.WriteLine("Hello. What's your name?");
+            Username = Console.ReadLine();
+
+            Console.WriteLine("Hi " + Username + "! How old are you?");
+            Console.WriteLine(Int32.TryParse(Console.ReadLine(), out age));
+            Console.WriteLine("So you're " + age + ", Eh? Thats not that old at all");
+            Console.ReadLine();
+
+            Console.WriteLine("So how much do you make, " + Username);
+            Double.TryParse(Console.ReadLine(), out price);
+            Console.WriteLine(price + "! I hope that's per hour and not per year!");
+
+
+
+
+        }
+    }
+}
